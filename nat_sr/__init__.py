bf61ef7b71a14a38186c690c4edc875a9f128882
Jonas Kubilius <qbilius@gmail.com>
2021-03-04 16:54:03 +0200

    cleaner API

--- nat_sr/__init__.py
+++ nat_sr/__init__.py
@@ -17,24 +17,20 @@ class Upsampler:
         self.sess = tf.Session()
         saver.restore(self.sess, 'resources/NatSR')
 
-    def __call__(self, inp, output_path=None):
-        if isinstance(inp, (str, Path)):
-            im = imageio.imread(inp)
-            ims = im[None,:,:,:]
-        else:
-            ims = np.array(inp)
-
-        ims = ims.astype(np.float32)
+    def __call__(self, ims):
+        ims = np.array(ims).astype(np.float32)
+        if ims.ndim == 3:
+            ims = ims[None,:,:,:]
         if np.max(ims) > 1:
             ims /= 255
 
         outims = self.sess.run(self.output, feed_dict={self.input: ims})
         outims = np.uint8(np.round(np.clip(outims * 255, 0, 255)))
 
-        if output_path is not None and len(outims) == 1:
-            imageio.imsave(output_path, outims[0])
         return outims
 
 
 if __name__ == '__main__':
-    Upsampler()('imageio:astronaut.png', 'astronaut4x.png')
+    ims = imageio.imread('imageio:astronaut.png')
+    out = Upsampler()(ims)
+    imageio.imwrite('astronaut4x.png', out[0])
